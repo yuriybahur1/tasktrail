0d1b8462c61d96b5cc23523af601a4b421e595a8
Yurii Bakhur <yuriybahur4@gmail.com>
2026-07-24 19:35:21 +0300

    ;

--- src/tasktrail/commands/project.py
+++ src/tasktrail/commands/project.py
@@ -10,4 +10,13 @@ def _handler(args: argparse.Namespace):
 def register(subparsers: Subparsers) -> None:
     parser = subparsers.add_parser("project")
 
+    commands = parser.add_subparsers(
+        dest="project_command",
+        required=True,
+    )
+
+    add_parser = commands.add_parser("add")
+    add_parser.add_argument("name")
+    add_parser.add_argument("--description")
+
     parser.set_defaults(handler=_handler)
