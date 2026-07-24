424065fca4af2ea11262166a384e68002b717678
Yurii Bakhur <yuriybahur4@gmail.com>
2026-07-24 20:47:40 +0300

    ;

--- src/tasktrail/commands/project.py
+++ src/tasktrail/commands/project.py
@@ -13,6 +13,8 @@ def _handler(args: argparse.Namespace):
         case "list":
             for x in services.list_projects(args.config.path, args.include_archived):
                 print(formatting.project_line(x))
+        case "archive":
+            pass
         case _:
             pass
 
@@ -31,5 +33,8 @@ def register(subparsers: Subparsers) -> None:
 
     list_parser = commands.add_parser("list")
     list_parser.add_argument("--include-archived", action="store_true")
+
+    archive_parser = commands.add_parser("archive")
+    archive_parser.add_argument("project_id", type="int")
 
     parser.set_defaults(handler=_handler)
